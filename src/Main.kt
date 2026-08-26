8440d0811dc15da0174ed5f7126c9c5673d85f79
omartinez37 <omartinez37@alumnos.uaq.mx>
2026-08-26 08:49:14 -0600

    Inicios de la rama calculos de vuelo

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,14 @@ fun main() {
    println("SIMULADOR DE MISIÓN DE RESCATE")
 
    println("Introdusca la distancia del recorrido de ida:")
+
+   println("Introdusca Peso de la carga:")
+
+   println("Introdusca la Batería disponible:")
+
+   println("Introdusca la Velocidad del viento:")
+
+   println("Introdusca Tipo de carga:")
+
+   println("Introdusca la condición del vuelo:")
 }
