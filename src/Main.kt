194e8b9056fc9615fb9325e0159f7b3d450a8069
omartinez37 <omartinez37@alumnos.uaq.mx>
2026-08-31 10:08:23 -0600

    Agrega funciones compactas de distancia y tiempo.

--- src/Main.kt
+++ src/Main.kt
@@ -13,3 +13,10 @@ fun main() {
 
    println("Introdusca la condición del vuelo:")
 }
+
+// Calculo de distancia total (ida y regreso)
+fun calcularDistanciaTotal(distanciaIda: Double): Double = distanciaIda * 2.0
+
+// Calculo de tiempo base (2 km por minuto)
+fun calcularTiempoBase(distanciaTotal: Double): Double = distanciaTotal / 2.0
+
