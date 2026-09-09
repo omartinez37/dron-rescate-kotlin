5be0ff67ebafa844c5b82c55d91cac2b184e6174
daniela <dmanriquez08@alumnos.uaq.mx>
2026-09-09 00:14:25 -0600

    Agrega cálculo de consumo y reserva de batería

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,21 @@
 package src
 
-fun main() {
-   println("SIMULADOR DE MISIÓN DE RESCATE")
+fun calcularConsumoBateria(
+   distanciaTotal: Double,
+   pesoCarga: Double,
+   consumoExtra: Double = 0.0
+): Double {
+   val consumoPorDistancia = distanciaTotal * 4
+   val consumoPorPeso = pesoCarga * 2
+   return consumoPorDistancia + consumoPorPeso + consumoExtra
 }
+
+fun calcularBateriaFinal(
+   bateriaInicial: Double,
+   consumo: Double
+): Double = bateriaInicial - consumo
+
+fun tieneReservaSuficiente(
+   bateriaFinal: Double,
+   reservaMinima: Double = 15.0
+): Boolean = bateriaFinal >= reservaMinima
